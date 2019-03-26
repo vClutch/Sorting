f2e1c81b1908c323b5f4465e00e559a255753c43
sk1357 <sk1357@CSELAB02-28.bennett.edu.in>
2019-03-26 14:40:39 +0530

    Bubble sort

---
+++ src/BubbleSort.java
@@ -0,0 +1,13 @@
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=1; 
+		int b=2;
+		int c=a+b;
+		System.out.print(c);
+		
+	}
+
+}
