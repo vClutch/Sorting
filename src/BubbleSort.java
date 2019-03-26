fe5bebe26a91eac54948211477941053f3dfc1c8
sk1357 <sk1357@CSELAB02-28.bennett.edu.in>
2019-03-26 15:05:11 +0530

    New Sort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,13 +1,39 @@
 
-public class BubbleSort {
+public class BubbleSort
+		{
+		    void bubbleSort(int arr[])
+		    {
+		    	// TODO Auto-generated method stub
+		        int n = arr.length;
+		        for (int i = 0; i < n-1; i++)
+		            for (int j = 0; j < n-i-1; j++)
+		                if (arr[j] > arr[j+1])
+		                {
+		                    int temp = arr[j];
+		                    arr[j] = arr[j+1];
+		                    arr[j+1] = temp;
+		                }
+		    }
+		 
+		    void printArray(int arr[])
+		    {
+		        int n = arr.length;
+		        for (int i=0; i<n; ++i)
+		            System.out.print(arr[i] + " ");
+		        System.out.println();
+		    }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int a=1; 
-		int b=2;
-		int c=a+b;
-		System.out.print(c);
+		    public static void main(String args[])
+		    {
+		        BubbleSort ob = new BubbleSort();
+		        int arr[] = {64, 34, 25, 12, 22, 11, 90};
+		        ob.bubbleSort(arr);
+		        System.out.println("Sorted array");
+		        ob.printArray(arr);
+		    }
+		
+		
 		
 	}
 
-}
+
